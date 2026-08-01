216f5282816c034a6226dacacdd01a80ee8cac42
per1234 <accounts@perglass.com>
2026-08-01 03:45:02 -0700

    Discover projects in fork repositories
    Arduino boards platform projects are often used as the starting point for other novel platforms.
    
    Previously, the search query was limited to results in non-fork repositories. Adding additional search queries which
    return results from repositories formally identified as forks will result in more discoveries.
    
    It is true that many forks do not represent novel platforms, and thus this will surely produce additional invalid
    discoveries. However, the previous queries also return many invalid discoveries, so their existence is simply a fact of
    life. The previous queries actually produce many discoveries that are forks, but not identified as such by GitHub due to
    some factor of how the repository is maintained (e.g., downloading a copy of the platform and then checking that into a
    fresh repo instead of clicking the "fork" button, or using GitHub's feature to detach the fork from the parent). In this
    way, formally identified fork discoveries will actually be far easier to evaluate, since the user can clearly see the
    parent project and how the two differ.
    
    Forks that do not contain commits not present in the parent repository are excluded from the discoveries. This will
    prevent the inclusion of duplicates and leftover staging forks for changes already pushed upstream.

--- internal/request/github/ghrepo/ghrepo.go
+++ internal/request/github/ghrepo/ghrepo.go
@@ -2,8 +2,11 @@
 package ghrepo
 
 import (
+	"bytes"
 	"context"
 	"fmt"
+	"io"
+	"net/http"
 
 	gogithub "github.com/google/go-github/v79/github"
 	"github.com/per1234-org/ino-platform-discovery/internal/request/clients"
@@ -56,6 +59,15 @@ func Get(owner string, name string) (repo.Type, error) {
 	repo.ResolvedURL = *githubResponse.HTMLURL
 
 	if repo.Fork {
+		/*
+			From reading the code search documentation, we might be led to the conclusion this is not necessary:
+			https://docs.github.com/search-github/searching-on-github/searching-code#considerations-for-code-search
+			> Code in forks is only searchable if [...] the forked repository has at least one pushed commit after being
+			> created.
+
+			However, experiments prove that the code search API does return results for forks which are not ahead of the
+			parent. So it seems this information is either incorrect, or not applicable to the API.
+		*/
 		ahead, err := ahead(githubResponse)
 		if err != nil {
 			return repo, err
@@ -78,7 +90,7 @@ func ahead(getRepoResponse *gogithub.Repository) (bool, error) {
 		// E.g., https://api.github.com/repos/arduino/arduino-esp32/compare/espressif:master...arduino:master
 		base := fmt.Sprintf("%s:%s", *getRepoResponse.Parent.Owner.Login, *getRepoResponse.Parent.DefaultBranch)
 		head := fmt.Sprintf("%s:%s", *getRepoResponse.Owner.Login, *getRepoResponse.DefaultBranch)
-		githubResponse, _, err := clients.Clients.GitHub.Repositories.CompareCommits(
+		githubResponse, httpResponse, err := clients.Clients.GitHub.Repositories.CompareCommits(
 			context.Background(),
 			*getRepoResponse.Owner.Login,
 			*getRepoResponse.Name,
@@ -88,6 +100,12 @@ func ahead(getRepoResponse *gogithub.Repository) (bool, error) {
 		)
 
 		if err != nil {
+			if noCommonAncestor(httpResponse.Response) {
+				logrus.Trace("No common ancestor between fork and parent.")
+				ahead = true
+				break
+			}
+
 			err := github.HandleRateLimiting(err)
 			if err != nil {
 				// Error is not recoverable.
@@ -105,4 +123,18 @@ func ahead(getRepoResponse *gogithub.Repository) (bool, error) {
 	}
 
 	return ahead, nil
+}
+
+// noCommonAncestor determines whether the HTTP response object is for the non-fatal "No common ancestor..." error.
+func noCommonAncestor(response *http.Response) bool {
+	if response.StatusCode != 404 {
+		return false
+	}
+
+	body, err := io.ReadAll(response.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	return bytes.Contains(body, []byte("No common ancestor between"))
 }
